31eb8d2290421dccad54a528b69a2bd65b894dcc
Gustavo Prardo <130465875+DevBaraka@users.noreply.github.com>
2023-08-11 16:12:52 -0300

    1 commit

---
+++ script.js
@@ -0,0 +1,19 @@
+const targetDate = new Date("2023-09-09T00:00:00").getTime();
+
+function updateCountdown() {
+  const currentDate = new Date().getTime();
+  const timeLeft = targetDate - currentDate;
+
+  const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+
+  document.getElementById("days").textContent = days < 10 ? "0" + days : days;
+  document.getElementById("hours").textContent = hours < 10 ? "0" + hours : hours;
+  document.getElementById("minutes").textContent = minutes < 10 ? "0" + minutes : minutes;
+  document.getElementById("seconds").textContent = seconds < 10 ? "0" + seconds : seconds;
+}
+
+updateCountdown();
+setInterval(updateCountdown, 1000);
